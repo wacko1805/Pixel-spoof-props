f5ca012c749543844c53bb1409d3a2877aef44c6
Jack Sam <54708993+wacko1805@users.noreply.github.com>
2021-06-15 14:49:08 +1000

    V.JUN.6TH.21

--- customize.sh
+++ customize.sh
@@ -1,3 +1,3 @@
-ui_print "- Installing Pixel 5 Device changer Props (Mar 2021)"
-ui_print "- By Wacko, reddit, telegram: wacko1805"
+ui_print "- Installing Pixel 5 Device changer Props (Jun 2021)"
+ui_print "- By Wacko1805, reddit + telegram: wacko1805"
 ui_print "- Visit my site for more: Pixel-fy.com/spoof"
